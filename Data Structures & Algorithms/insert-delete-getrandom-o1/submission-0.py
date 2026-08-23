d37c159ad92b412719130aa46e6ac6b19865c312
Creighton Cornelison <creightoncornelison@gmail.com>
2026-08-22 20:46:37 -0500

    Add: insert-delete-getrandom-o1 - submission-0

---
+++ Data Structures & Algorithms/insert-delete-getrandom-o1/submission-0.py
@@ -0,0 +1,28 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.items = set()
+
+    def insert(self, val: int) -> bool:
+        if val in self.items:
+            return True
+        self.items.add(val)
+        return False
+
+    def remove(self, val: int) -> bool:
+        if val in self.items:
+            self.items.remove(val)
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        elements = list(self.items)
+
+        return elements[(random.randrange(0, len(elements)))]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
