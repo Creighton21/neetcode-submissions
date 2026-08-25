bacb2f49aa98f0487160545bdbbfc3e14015f26a
Creighton Cornelison <creightoncornelison@gmail.com>
2026-08-25 07:39:14 -0500

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.py
@@ -0,0 +1,45 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = {}
+        self.rank = {}
+
+        for i in range(1, n+1):
+            self.parent[i] = i
+            self.rank[i] = 0
+
+    def find(self, n):
+        if n != self.parent[n]:
+            self.parent[n] = self.find(self.parent[n])
+        return self.parent[n]
+
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+
+        if p1 == p2:
+            return False
+        
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = p1
+        elif self.rank[p2] > self.rank[p1]:
+            self.parent[p1] = p2
+        else:
+            self.parent[p1] = p2
+            self.rank[p2] += 1
+        return True
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        union = UnionFind(len(edges))
+
+        for edge in edges:
+            did_union = union.union(edge[0], edge[1])
+
+            if not did_union:
+                return edge
+
+        return [0,0]
+
+
+
+        
