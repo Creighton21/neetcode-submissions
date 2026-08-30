413969c509119a81f7dcb7621273956b62233a9c
Creighton Cornelison <creightoncornelison@gmail.com>
2026-08-30 09:17:57 -0500

    Add: clone-graph - submission-5

---
+++ Data Structures & Algorithms/clone-graph/submission-5.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        stack = [node] # frontier
+        copy = {node: Node(node.val)} # discovery
+
+        while stack:
+            original = stack.pop()
+
+            for neighbor in original.neighbors:
+                if neighbor not in copy:
+                    copy[neighbor] = Node(neighbor.val) # Discover
+                    stack.append(neighbor) # Push to the stack
+
+                copy[original].neighbors.append(copy[neighbor]) # Add the neighbor
+
+        return copy[node]
